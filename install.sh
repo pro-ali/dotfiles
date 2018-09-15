08ce6947e6003370530dd41795d9119373335f43
pro-ali <uk.pro.ali@gmail.com>
2018-09-15 14:39:46 +0100

    vim: install.sh config commands added

--- install.sh
+++ install.sh
@@ -12,5 +12,12 @@
 sudo apt install curl
 mkdir -p nvim/dein/
 cp -r ./nvim/ ~/.config/nvim/
+sudo apt install neovim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./nvim/dein/installer.sh
 sh ./nvim/dein/installer.sh ~/.config/nvim/dein/
+
+# VIM
+mkdir ~/.vim/
+cp -r ./nvim/* ~/.vim/
+rm ~/.vim/init.vim
+cp ./nvim/init.vim ~/.vimrc
