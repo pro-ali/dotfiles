935320d3cdd49532a6b47ed469df5cec90ee7a5e
pro-ali <uk.pro.ali@gmail.com>
2018-09-18 18:20:32 +0100

    update.sh: bug fixing

--- utils/.util_constants.sh
+++ utils/.util_constants.sh
@@ -12,10 +12,12 @@
     DOTFILES_ROOT_DIR="$PWD"
 
 # VIM PACKAGE CONSTANTS -----------------------------------------------------
-	VIMRC_REPO_FILE_PATH="./vim/.vimrc"
 	VIMRC_TARGET_FILE_PATH="$HOME/.vimrc"
-	VIM_REPO_DIR_PATH="./vim/.vim"
-	VIM_TARGET_DIR_PATH="$HOME/.vimrc"
+	VIMRC_REPO_FILE_PATH="./vim/.vimrc"
+
+	VIM_TARGET_DIR_PATH="$HOME/.vim"
+	VIM_REPO_DIR_PATH="/vim/.vim"
+
 	DEIN_CACHE_DIR="$VIM_TARGET_DIR_PATH/dein"
 	DEIN_INSTALLER_PATH="$DEIN_CACHE_DIR/installer.sh"
 	DEIN_INSTALLER_URL="https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh"
