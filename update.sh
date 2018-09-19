5d9bc52642ef2ec147e938693b0e4babd13cc5ff
pro-ali <uk.pro.ali@gmail.com>
2018-09-19 16:23:13 +0100

    vim: youcomplete me install commands

--- update.sh
+++ update.sh
@@ -21,6 +21,12 @@
 		#   DEIN_CACHE_DIR
 		#   DEIN_INSTALLER_URL
 		#   DEIN_INSTALLER_PATH
+		#   VALLORIC_REPO_DIR
+		#   YCM_REPO_DIR
+		#   YCM_DEPENDENCY_INSTALL_STRING
+		#   YCM_GIT_URL
+		#   YCM_GIT_REQUEST_DIR
+
 
 	source ./utils/.util_functions.sh
 		# USED FUNCTIONS
@@ -43,6 +49,7 @@ echo
 # CHECK AND INSTALL PROGRAMMES ---------------------------------------------
 	install_apt_command "tree"
 	install_apt_command "curl"
+	install_apt_command "git"
 
 
 # PACKAGES AND COMMANDS ----------------------------------------------------
@@ -51,18 +58,29 @@ echo
     replace_bashrc "$DOTFILES_ROOT_DIR"; echo
 
     print_title "# VIM PACKAGE -----------------------------------------------------------"
-	echo "1-- SYNC ~/.vimrc.."; sync_file 	$VIMRC_TARGET_FILE_PATH $VIMRC_REPO_FILE_PATH
-	echo "2-- SYNC ~/.vim/..";	 sync_dir 	$VIM_TARGET_DIR_PATH 	$VIM_REPO_DIR_PATH; echo
+	echo "1-- SYNC [$VIMRC_TARGET_FILE_PATH]" 	&& 	sync_file 	$VIMRC_TARGET_FILE_PATH $VIMRC_REPO_FILE_PATH; echo
+	echo "2-- SYNC [$VIM_TARGET_DIR_PATH]"		&&	sync_dir 	$VIM_TARGET_DIR_PATH 	$VIM_REPO_DIR_PATH; echo
+
 	echo "3-- CHECK DEIN INSTALLATION" &&
 		if [ ! -d "$DEIN_CACHE_DIR" ]; then
-			echo "   :INSTALLING DEIN TO: $DEIN_CACHE_DIR" &&
-				mkdir -p $DEIN_CACHE_DIR &&
-				curl $DEIN_INSTALLER_URL > $DEIN_INSTALLER_PATH &&
-				sh $DEIN_INSTALLER_PATH $DEIN_CACHE_DIR
-		else
-			echo "   :DEIN EXISTS SO NOT INSTALLING:"
-		fi; echo
+			echo &&
+				echo "[1.] DEIN-> MKDIR [$DEIN_CACHE_DIR]" 		&& 	mkdir -p $DEIN_CACHE_DIR &&
+				echo "[2.] DEIN-> DWNLD INSTALLER" 				&& 	curl $DEIN_INSTALLER_URL > $DEIN_INSTALLER_PATH &&
+				echo "[3.] DEIN-> RUN   INSTALLER" 				&& 	sh $DEIN_INSTALLER_PATH $DEIN_CACHE_DIR &&
+				echo
+		else echo "   :DEIN EXISTS SO NOT INSTALLING:"; fi; echo
+
+	echo "4-- CHECK YOUCOMPLETEME BUNDLE INSTALLATION" &&
+		if [ ! -d "$YCM_GIT_REQUEST_DIR" ]; then
+			echo &&
+				echo "[1.] YCM--> MKDIR [$VALLORIC_REPO_DIR]" 	&& 	(mkdir -p $VALLORIC_REPO_DIR || true) &&
+				echo "[2.] YCM--> CLANG DEPENDENCY INSTALL"		&& 	(eval $YCM_DEPENDENCY_INSTALL_STRING || true) &&
+				echo "[3.] YCM--> CLONE REPO TO DEIN DIR"		&& 	(git clone $YCM_GIT_URL $YCM_REPO_DIR || true) &&
+				echo "[4.] YCM--> PUSHD 2 [$YCM_REPO_DIR]"		&& 	cd $YCM_REPO_DIR &&
+				echo "[5.] YCM--> UPDATE SUBMODULES"			&& 	git submodule update --init --recursive &&
+				echo "[6.] YCM--> INSTALL YCM"					&& 	./installer.py --clang-completer &&
+				echo "[7.] YCM--> POPD 2 [$DOTFILES_ROOT_DIR]"	&& 	cd $DOTFILES_ROOT_DIR &&
+				echo
+		else echo "   :YOUCOMPLETEME EXISTS SO NOT INSTALLING:"; fi; echo
 
 
-# script not to be sourced
-exit 0
--- utils/.util_constants.sh
+++ utils/.util_constants.sh
@@ -12,14 +12,25 @@
     DOTFILES_ROOT_DIR="$PWD"
 
 # VIM PACKAGE CONSTANTS -----------------------------------------------------
+	# files
 	VIMRC_TARGET_FILE_PATH="$HOME/.vimrc"
 	VIMRC_REPO_FILE_PATH="./vim/.vimrc"
-
+	# dirs
 	VIM_TARGET_DIR_PATH="$HOME/.vim"
 	VIM_REPO_DIR_PATH="./vim/.vim"
 
+	# SOURCE https://github.com/Shougo/dein.vim#unixlinux-or-mac-os-x
 	DEIN_CACHE_DIR="$HOME/.dein"
 	DEIN_INSTALLER_PATH="$DEIN_CACHE_DIR/installer.sh"
 	DEIN_INSTALLER_URL="https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh"
+	DEIN_GITHUB_REPODIR="$DEIN_CACHE_DIR/repos/github.com"
+	
+	# SOURCE https://github.com/Valloric/YouCompleteMe#ubuntu-linux-x64
+	VALLORIC_REPO_DIR="$DEIN_CACHE_DIR/repos/Valloric"
+	YCM_REPO_DIR="$VALLORIC_REPO_DIR/YouCompleteMe"
+	YCM_GIT_REQUEST_DIR="$YCM_REPO_DIR/third_party/requests_futures/.git"
+	YCM_DEPENDENCY_INSTALL_STRING="sudo apt -y install build-essential cmake python-dev python3-dev"
+	YCM_GIT_URL="https://github.com/Valloric/YouCompleteMe.git"
+
 
 
